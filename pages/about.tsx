69c79536f974ffe72561c8d3683d738752e8d56d
Joona Piirainen <joona.piirainen@gmail.com>
2021-01-28 16:25:30 +0200

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,7 +13,7 @@ import {
 import DefaultLayout from '_layouts/default'
 import { GrGolang, GrReactjs } from 'react-icons/gr'
 import { IconType } from 'react-icons/lib'
-import { FaLanguage } from 'react-icons/fa'
+import { FaDatabase, FaLanguage } from 'react-icons/fa'
 import {
 	IoLogoJavascript,
 	IoLogoNodejs,
@@ -21,7 +21,17 @@ import {
 	IoIosBicycle,
 	IoMdBaseball,
 } from 'react-icons/io'
-import { SiTypescript, SiHaskell, SiClojure, SiPython, SiKubernetes } from 'react-icons/si'
+import {
+	SiTypescript,
+	SiHaskell,
+	SiClojure,
+	SiPython,
+	SiKubernetes,
+	SiRust,
+	SiPostgresql,
+	SiMongodb,
+	SiElasticsearch,
+} from 'react-icons/si'
 import { BiServer } from 'react-icons/bi'
 import { AiOutlineCloudServer } from 'react-icons/ai'
 import { ImPencil2 } from 'react-icons/im'
@@ -120,6 +130,13 @@ const ContactPage: React.FC = () => {
 						icon={IoLogoJavascript}
 					/>
 					<MyListItem
+						label={'Rust'}
+						desc={
+							'I wanted to try to learn a systems level programming language and Rust seemed like a cool choice. It is quite "functional programming" oriented and thats why I chose is over C and C++. I`ve built some small CLI:s with it and it seems like a good and super fast language for that. I have also planned to try it for some web development.'
+						}
+						icon={SiRust}
+					/>
+					<MyListItem
 						label={'Haskell'}
 						desc={
 							'Ok, so I´m definitely not an Haskell expert but I have been studying it quite a bit resently. I think it´s a very interesting language and I would love to get better at it.'
@@ -139,6 +156,37 @@ const ContactPage: React.FC = () => {
 							'I´m not going to say much about Python. I´m not an expert but I have done some scripting with it. Seems like a good language for getting stuff done quickly.'
 						}
 						icon={SiPython}
+					/>
+					<MyListItem
+						label={'Go'}
+						desc={
+							'Recently I started writing an api using go and I have to say that right now im absolutely loving it! The simplicity of the language is truly refreshing'
+						}
+						icon={GrGolang}
+					/>
+				</UnorderedList>
+				<Heading size={heading2Size} fontFamily={'main'} p={4}>
+					Databases <Icon as={FaDatabase} />
+				</Heading>
+				<UnorderedList spacing={3} p={2}>
+					<MyListItem
+						label={'PostgresSQL'}
+						desc={
+							'Postgres is propably the database I know the best. Although some people often talk smack about these classic sql dbs I think they are awesome!'
+						}
+						icon={SiPostgresql}
+					/>
+					<MyListItem
+						label={'Mongodb'}
+						desc={
+							'At my previous job we used for mongodb for everything in the persistence layer and it was fine I guess. Don`t have much to say about this.'
+						}
+						icon={SiMongodb}
+					/>
+					<MyListItem
+						label={'ElasticSearch'}
+						desc={'Quite self explanitory. Good for searching stuff.'}
+						icon={SiElasticsearch}
 					/>
 				</UnorderedList>
 				<Heading size={heading2Size} fontFamily={'main'} p={4}>
@@ -163,13 +211,6 @@ const ContactPage: React.FC = () => {
 							'NodeJs is the only serverside technology I have used but I have really liked it. It´s quite nice to be able to develop both bacend and frontend apps with the same language!'
 						}
 						icon={IoLogoNodejs}
-					/>
-					<MyListItem
-						label={'Go microcervices'}
-						desc={
-							'Recently I started writing an api using go and I have to say that right now im absolutely loving it! The simplicity of the language is truly refreshing'
-						}
-						icon={GrGolang}
 					/>
 				</UnorderedList>
 				<Heading size={heading2Size} fontFamily={'main'} p={4}>
